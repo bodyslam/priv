192a5552a2765f89b4db56f6b516014596be2849
bodyslam <thomastenwong@gmail.com>
2017-08-22 17:30:13 -0400

    WIP

--- convertDB.js
+++ convertDB.js
@@ -3,13 +3,17 @@
 var mongo = require('mongodb').MongoClient
 var Promise = require('bluebird')
 var url = 'mongodb://localhost:27017/priv'
+var ObjectID = require('mongodb').ObjectID
 var db
 
 connect()
-.then(updateGeocoords)
+.then(getAll)
+.then(updateAll)
 .then(r => {
-  db.close
+  db.close()
+  console.log(r)
   console.log("FIN")
+  //process.exit()
 })
 
 function connect () {
@@ -33,6 +37,25 @@ function getAll () {
 
 function updateAll(data) {
   return Promise.all(data.map(venue => {
-    
-  }))
+    console.log("==>OBJECT ID?", venue._id)
+    db.collection('venues').findOneAndUpdate(
+      {"_id": ObjectID(venue._id)},
+      {
+        $set: {
+          "address.location": {
+            "type": "Point",
+            "coordinates" : [venue.address.coord[0], venue.address.coord[1] ]
+          }
+        }
+      },
+      (err, oldVenue) => {
+        if (err) {
+          console.error("ERRR===>", err)
+          process.exit()
+          return Promise.reject(err)
+        }
+        console.log("==> REsolving", oldVenue)
+        return Promise.resolve(oldVenue)
+      }
+    )}))
 }
